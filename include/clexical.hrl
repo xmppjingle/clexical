4328218d13f7923626b7e2fbc953c75a1e1c851c
xmppjingle <barata7@gmail.com>
2016-05-18 23:43:32 -0300

    Format Improvement

--- include/clexical.hrl
+++ include/clexical.hrl
@@ -2,5 +2,17 @@
 
 -type kind() :: preposition | verb.
 
--record(predicate, {id = <<>> :: binary(), subject = <<>> :: binary(), action :: {kind(), binary()}, adjectives = undefined :: dict()|undefined, abstract = undefined :: any(), author = undefined :: any()}).
--record(letter, {predicates = [] :: [#predicate{}], author :: any(), type :: decree|bulletin}).
+-record(predicate, {
+	id = <<>> :: binary(),
+	subject = <<>> :: binary(), 
+	action :: {kind(), binary()}, 
+	adjectives = undefined :: dict()|undefined, 
+	abstract = undefined :: any(), 
+	author = undefined :: any()
+	}).
+
+-record(letter, {
+	predicates = [] :: [#predicate{}], 
+	author :: any(), 
+	type :: decree|bulletin
+	}).
